9c83a12023f635ac6bbb2149b6ab2d6e96f81e91
Code-xed <ngudulu@gmail.com>
2024-02-04 03:43:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const answers = ["No", "Are you sure?", "Really sure?", "Are you positive?", "Pookie Please", "Just think about it", "If you say no, I'll be very sad", "I'll be very very sad", "I'll be very very very very sad", "Fine, I'll stop asking!", "Just kdding, PLEASE SAY YES!!", "I'll be very very very very very very sad", "You're breaking my heart :("];
-let counter = 1;
 
 const image = document.getElementById('gif');
 const heading = document.getElementById('headline');
@@ -7,16 +6,23 @@ const YES = document.getElementById('yes');
 const NO = document.getElementById('no');
 const buttons = document.getElementsByClassName('button-container')[0].classList;
 console.log(buttons);
+
 const w_factor = 4;
 const h_factor = 3;
-const f_factor = 0.1;
+const audio = new Audio("./static/meme.mp3");
+
+let counter = 1;
+
+function playAudio() {
+  audio.currentTime = 0;
+  audio.play();
+}
 
 function Answer(answer) {
   const style = window.getComputedStyle(YES);
   let width = style.getPropertyValue("width");
   let height = style.getPropertyValue("height");
-  let fontsize = style.getPropertyValue('font-size');
-  fontsize = parseInt(fontsize.replace('px', ''));
+
   width = parseInt(width.replace('px', ''));
   height = parseInt(height.replace('px', ''));
 
@@ -30,16 +36,12 @@ function Answer(answer) {
     heading.innerHTML = "OkYAwww! YAY!!!";
   }
   else {
-    console.log(fontsize);
+    playAudio();
     width += w_factor;
     height += h_factor;
-    fontsize += f_factor;
     YES.style.width = width + "px";
     YES.style.height = height + "px";
-    console.log(fontsize);
-    YES.style.fontSize = fontsize + "px";
     NO.innerHTML = answers[counter];
-
     counter += 1;
   }
 }
